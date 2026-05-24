2c726de236a14b6712612365ba66918ed3562548
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-05-24 13:57:03 +0200

    Create answer list component

---
+++ Quiz.Student/Presentation/QuizSession/AnswerList.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace QuizApp.Student.Presentation.QuizSession
+{
+    partial class AnswerList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // AnswerList
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            Name = "AnswerList";
+            Size = new Size(32, 32);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Quiz.Student/Presentation/QuizSession/AnswerList.cs
@@ -0,0 +1,135 @@
+﻿using QuizApp.Core.Domain;
+using System.ComponentModel;
+using System.Data;
+
+namespace QuizApp.Student.Presentation.QuizSession;
+
+public partial class AnswerList : UserControl
+{
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+    public IReadOnlyList<Answer> Answers
+    {
+        get;
+        set { field = value; UpdateAnswerList(); }
+    } = [];
+
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    public IReadOnlyList<Answer> SelectedAnswers
+    {
+        get => GetSelectedAnswers();
+        set => SetSelectedAnswers(value);
+    }
+
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    public bool Marked
+    {
+        get;
+        set { field = value; UpdateColorMarking(); }
+    }
+
+    public event Action? SelectedAnswersChanged;
+
+    public AnswerList()
+    {
+        InitializeComponent();
+    }
+
+    private void UpdateAnswerList()
+    {
+        SuspendLayout();
+
+        Controls.Clear();
+
+        foreach (Answer answer in Answers.Reverse())
+        {
+            CheckBox checkbox = CreateAnswerCheckbox(answer);
+            checkbox.CheckedChanged += OnAnswerCheckedChanged;
+            Controls.Add(checkbox);
+        }
+
+        UpdateColorMarking();
+
+        ResumeLayout();
+    }
+
+    private CheckBox CreateAnswerCheckbox(Answer answer)
+    {
+        const int margin = 3;
+        const int padding = 8;
+
+        CheckBox checkbox = new()
+        {
+            Appearance = Appearance.Button,
+            AutoEllipsis = true,
+            Dock = DockStyle.Top,
+            Margin = new Padding(margin),
+            Padding = new Padding(padding),
+            Parent = this,
+            Tag = answer,
+            Text = answer.Title,
+            UseVisualStyleBackColor = true,
+        };
+
+        checkbox.Height = (2 * margin) + (2 * padding) + checkbox.Font.Height;
+
+        return checkbox;
+    }
+
+    private void OnAnswerCheckedChanged(object? sender, EventArgs e)
+    {
+        SelectedAnswersChanged?.Invoke();
+    }
+
+    private IReadOnlyList<Answer> GetSelectedAnswers()
+    {
+        return [.. Controls.OfType<CheckBox>()
+            .Where(it => it.Checked)
+            .Select(it => it.Tag as Answer)
+            .OfType<Answer>()];
+    }
+
+    private void SetSelectedAnswers(IReadOnlyList<Answer> selectedAnswers)
+    {
+        foreach (CheckBox checkbox in Controls.OfType<CheckBox>())
+        {
+            if (checkbox.Tag is not Answer answer)
+            {
+                continue;
+            }
+            checkbox.Checked = selectedAnswers.Contains(answer);
+        }
+    }
+
+    private void UpdateColorMarking()
+    {
+        SuspendLayout();
+
+        foreach (CheckBox checkbox in Controls.OfType<CheckBox>())
+        {
+            if (checkbox.Tag is not Answer answer)
+            {
+                continue;
+            }
+
+            Color color = Marked
+                ? GetAnswerColor(checkbox.Checked, answer.IsCorrect)
+                : Color.Empty;
+
+            checkbox.BackColor = color;
+            checkbox.UseVisualStyleBackColor = color == Color.Empty;
+        }
+
+        ResumeLayout();
+    }
+
+    private Color GetAnswerColor(bool isChecked, bool isCorrect)
+    {
+        return (isChecked, isCorrect) switch
+        {
+            (true, true) => Color.FromArgb(213, 245, 227),
+            (true, false) => Color.FromArgb(242, 212, 209),
+            (false, true) => Color.FromArgb(234, 250, 244),
+            _ => Color.Empty,
+        };
+    }
+}
